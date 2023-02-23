5cdc38a31381dc00e706812bd3bf0e37bf663b39
IlmirSalim <salimov_ilmir90@mail.ru>
2023-02-23 19:41:30 +0500

    add delete post

--- src/pages/posts/detail/styled.js
+++ src/pages/posts/detail/styled.js
@@ -18,4 +18,59 @@ export const LinkWrapper = styled.div`
     display: flex;
     justify-content: center;
     gap: 15px;
+    padding-top: 15px;
+    align-items: center;
+`
+export const DeleteButton = styled.button`
+    border: 1px solid;
+    background: white;
+    padding: 5px 10px;
+    color: black;
+    border-radius: 10px;
+    cursor: pointer;
+
+    &:hover {
+        background: red;
+        color: white;
+        border: 1px solid black;
+    }
+`
+
+export const ModalWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgb(0, 0, 0, 0.5);
+    z-index: 1;
+`
+
+export const Modal = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    color: black;
+    border: 1px solid black;
+    padding: 10px 20px;
+    background: white;
+    border-radius: 25px;
+`
+
+export const Modaltext = styled.div`
+    color: black;
+    font-weight: bold;
+    font-size: 20px;
+    text-align: center;
+`
+
+export const ModalContent = styled.div`
+    display: flex;
+    gap: 15px;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding-top: 10px;
 `
